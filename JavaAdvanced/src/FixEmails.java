68d0e8fa357c07a5b1cdad50241bbca89c7c527c
Erlandas <3rlandas@gmail.com>
2022-12-27 16:44:10 +0000

    finished

--- JavaAdvanced/src/FixEmails.java
+++ JavaAdvanced/src/FixEmails.java
@@ -1,3 +1,5 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class FixEmails {
@@ -5,8 +7,27 @@ public class FixEmails {
 
         Scanner scanner = new Scanner(System.in);
 
+        LinkedHashMap<String, String> data = new LinkedHashMap<>();
+        String name = scanner.nextLine();
+
+        while (!name.equals("stop")) {
+
+            String emailInput = scanner.nextLine();
 
 
+            if (!emailInput.toLowerCase().contains(".uk") && !emailInput.toLowerCase().contains(".us") && !emailInput.toLowerCase().contains(".com")) {
 
+                if (data.containsKey(name)) {
+                    data.replace(name, data.get(emailInput));
+                }
+                data.putIfAbsent(name, emailInput);
+            }
+            name = scanner.nextLine();
+        }
+
+        for (Map.Entry<String, String> datum : data.entrySet()) {
+
+            System.out.printf("%s -> %s\n", datum.getKey(), datum.getValue());
+        }
     }
 }
