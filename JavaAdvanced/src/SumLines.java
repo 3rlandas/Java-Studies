c048abae9f4c2cfff1231050fa458c41c98eb4a4
Erlandas <3rlandas@gmail.com>
2023-01-22 12:55:42 +0000

    finished

--- JavaAdvanced/src/SumLines.java
+++ JavaAdvanced/src/SumLines.java
@@ -1,8 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class SumLines {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        String inputPath = "";
+        String inputPath = "C:\\Coding\\Java-Studies\\JavaAdvanced\\resources\\04-Java-Advanced-Files-and-Streams-Exercise-Resources\\input.txt";
 
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(inputPath));
 
+            String line = bufferedReader.readLine();
+            while (line != null) {
+
+                int lines = 0;
+
+                for (char c : line.toCharArray()) {
+
+                    lines += c;
+                }
+
+                System.out.println(lines);
+                line = bufferedReader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
