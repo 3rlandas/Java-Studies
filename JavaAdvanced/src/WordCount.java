12c00d9efab05419d5538a8cb294bf594d1d7d2c
Erlandas <3rlandas@gmail.com>
2023-01-23 20:13:33 +0000

    finished

--- JavaAdvanced/src/WordCount.java
+++ JavaAdvanced/src/WordCount.java
@@ -1,31 +1,53 @@
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 public class WordCount {
     public static void main(String[] args) {
 
         String path = "C:\\Coding\\Java-Studies\\JavaAdvanced\\resources\\04-Java-Advanced-Files-and-Streams-Exercise-Resources\\text.txt";
-        String searchedWords = "C:\\Coding\\Java-Studies\\JavaAdvanced\\resources\\04-Java-Advanced-Files-and-Streams-Exercise-Resources\\words.txt";
+        String providedWordsPath = "C:\\Coding\\Java-Studies\\JavaAdvanced\\resources\\04-Java-Advanced-Files-and-Streams-Exercise-Resources\\words.txt";
         String outputPath = "C:\\Coding\\Java-Studies\\JavaAdvanced\\resources\\04-Java-Advanced-Files-and-Streams-Exercise-Resources\\WordCountOutput";
 
-        LinkedHashMap<String, Integer> wordCount = new LinkedHashMap<>();
+        Map<String, Integer> providedWords = new LinkedHashMap<>();
 
         try {
-            List<String> lines = Files.readAllLines(Path.of(searchedWords));
+            PrintWriter printWriter = new PrintWriter(new FileWriter(outputPath));
+
+            List<String> lines = Files.readAllLines(Path.of(providedWordsPath));
 
             for (String line : lines) {
+                String[] arrayList = line.split("\\s+");
 
-                if()
+                for (String s : arrayList) {
 
+                    providedWords.putIfAbsent(s, 0);
+                }
             }
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            List<String> linesFromPath = Files.readAllLines(Path.of(path));
 
+            for (String line : linesFromPath) {
+
+                String[] arrayList = line.split("\\s+");
+                for (String s : arrayList) {
+
+                    if (providedWords.containsKey(s)) {
+
+                        providedWords.put(s, providedWords.get(s) + 1);
+                    }
+                }
+            }
+            for (Map.Entry<String, Integer> entry : providedWords.entrySet()) {
+
+                printWriter.printf("%s - %d%n", entry.getKey(), entry.getValue());
+
+            }
+            printWriter.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
