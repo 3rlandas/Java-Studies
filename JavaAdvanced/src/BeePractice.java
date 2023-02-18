6518074543c20435528d4384db8ac9feb125df2d
Erlandas <3rlandas@gmail.com>
2023-02-18 10:31:59 +0000

    finished

---
+++ JavaAdvanced/src/BeePractice.java
@@ -0,0 +1,30 @@
+/*public class BeePractice {
+
+
+    public class BeeGameGrid
+    {
+        private string[][] _grid;
+        private int _currentRow;
+        private int _currentColumn;
+        public int FlowersCollected;
+
+        public BeeGameGrid(string[][] initializedGrid, int currentRow = 0, int currentColumn = 0)
+        {
+            _grid = initializedGrid;
+            _currentRow = currentRow;
+            _currentColumn = currentColumn;
+        }
+
+
+        public bool MoveBee(string direction)
+        {
+            bool isOutOfBounds = false;
+
+            //Logic using given direction
+            //Update as needed CurrentRow and CurrentColumn
+            //Set FlowersCollected
+
+            return isOutOfBounds;
+        }
+    }
+}*/
